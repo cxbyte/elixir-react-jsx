de650289f5c76dd07090208bb9f8948f5fa95dba
Andrey Petrov <apetrov2@avito.ru>
2016-01-08 14:07:00 +0300

    elixir 3.0 support

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var react = require('gulp-react');
 var concat = require('gulp-concat');
+var task = elixir.Task;
 
-elixir.extend("jsx", function(src, dest) {
+elixir.extend("jsx", function (src, dest) {
     src = src || 'resources/assets/jsx/*.jsx';
     dest = dest || 'public/js';
 
     var doConcat = ~dest.indexOf('.js');
 
-    gulp.task("jsx", function() {
-        gulp.src(src)
+    new task('jsx', function () {
+        return gulp.src(src)
             .pipe(react())
             .pipe(gulpif(doConcat, concat(dest)))
             .pipe(gulp.dest(dest));
     });
-
-    return this.queueTask("jsx");
 });
